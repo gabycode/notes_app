0bbe03ba33f05718a2f8932367dfd629b09b073b
gabycode <86427446+gabycode@users.noreply.github.com>
2024-02-20 22:10:28 -0400

    add: add note functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,49 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [notes, setNotes] = useState([]);
+  const [noteTitle, setNoteTitle] = useState("");
+  const [noteBody, setNoteBody] = useState("");
+  const [notesList, setNotesList] = useState([]);
 
-  console.log(notes);
+  const addNote = (e) => {
+    e.preventDefault();
+    const newNote = {
+      title: noteTitle,
+      body: noteBody,
+    };
+    setNotesList([...notesList, newNote]);
+    setNoteTitle("");
+    setNoteBody("");
+  };
 
   return (
     <div className="app">
       <div className="left-sidebar">
-        <form className="add-note">
+        <form className="add-note" onSubmit={addNote}>
           <input
+            value={noteTitle}
             type="text"
             placeholder="Write a title"
             className="note-title"
+            onChange={(e) => setNoteTitle(e.target.value)}
           />
-          <textarea placeholder="Write here..." className="note-body" />
+          <textarea
+            value={noteBody}
+            placeholder="Write here..."
+            className="note-body"
+            onChange={(e) => setNoteBody(e.target.value)}
+          />
           <button type="submit">Add note</button>
         </form>
       </div>
       <div className="notes-ui">
         <div className="notes-container">
-          <h2>Hello</h2>
-          <p>This is a note</p>
+          {notesList.toReversed().map((note, id) => (
+            <div key={id} className="note">
+              <h2>{note.title}</h2>
+              <p>{note.body}</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
